353b8bacacfa1e772f1802e6e90cb13c079cc226
Jan Deters <jan@MacBook-Pro.fritz.box>
2020-08-07 14:59:35 +0200

    Fix warnings

--- src/cell.js
+++ src/cell.js
@@ -21,7 +21,6 @@ export default class Cell extends React.Component{
         let classes = `cell ${living ? "cell--alive" : "cell--dead"}`;
         let xPosition = this.state.x * 12;
         let yPosition = this.state.y * 12;
-        let style = `left: ${ xPosition}px top: ${yPosition}px`;
         
         return(
             <div className={classes} style={{top: yPosition + 'px', left: xPosition + 'px' }} onClick={() => this.props.clickFunction(this.props.x, this.props.y)}>
